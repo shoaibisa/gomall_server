bc3e7625a9755b41cc142cf88c1fe3f090ad16ae
shoaibisa <shoaibisa1@gmail.com>
2023-11-04 15:46:30 +0530

    new

--- server_go.js
+++ server_go.js
@@ -32,7 +32,7 @@ app.get("/", async (req, resq, next) => {
 //  Routes
 app.use(proute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5100;
 app.listen(PORT, () => {
   console.log(`Server is Runing on Port ${PORT}`);
 });
